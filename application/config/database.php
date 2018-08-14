ade05f1bb1853ec1c9b34ec3642c5c51966b4f01
HarshGundecha <harsh.gundecha@gmail.com>
2018-08-14 19:06:08 +0530

    configured database connection

--- application/config/database.php
+++ application/config/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn'	=> 'mysql:host=localhost;dbname=codeigniter_crud_product',
 	'hostname' => 'localhost',
-	'username' => '',
+	'username' => 'root',
 	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
+	'database' => 'codeigniter_crud_product',
+	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
