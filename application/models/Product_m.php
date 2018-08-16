e5c4b67c26f804400ee0d6f1fa9026e09627de50
HarshGundecha <harsh.gundecha@gmail.com>
2018-08-17 00:23:24 +0530

    added query for adding product

--- application/models/Product_m.php
+++ application/models/Product_m.php
@@ -9,9 +9,22 @@ class Product_m extends MY_Model{
     $data = $this->db
       ->select('P.Name PName, P.Description, P.Price, C.Name CName')
       ->from('product P')
-      ->join('category C', 'P.CategoryID=C.CategoryID')
+      ->join('category C', 'P.CategorySlug=C.CategorySlug')
       ->get()
       ->result();
     return $data;
+  }
+
+  public function add_product_d($data)
+  {
+    $this->db
+      ->insert('product', $data);
+  }
+
+  public function get_category_d()
+  {
+    return $this->db
+      ->get('category')
+      ->result();
   }
 }
