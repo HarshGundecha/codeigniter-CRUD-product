d1285b3e827bd1341c3ee693faf8e8d74b49e7ca
HarshGundecha <harsh.gundecha@gmail.com>
2018-08-17 00:17:44 +0530

    added code for adding product with validation

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -4,13 +4,15 @@ class Product extends CI_Controller {
 	{
 		Parent::__construct();
 		$this->load->model('Product_m', 'pm');
+		$this->load->library('form_validation', '','fv');
 	}
 	public function index()
 	{
 
 		//echo '<pre>';
 		//print_r($data);;
-		$this->load->view('product');
+		$data['cat_data'] = $this->pm->get_category_d();
+		$this->load->view('product',$data);
 	}
 
 	public function get_product($slug=false)
@@ -28,6 +30,29 @@ class Product extends CI_Controller {
       "iTotalDisplayRecords" => count($data),
       "aaData"=>$data
     ]);
+	}
+
+	public function add_product()
+	{
+		$this->fv->set_rules('aName', 'Product Name', 'trim|required');
+		$this->fv->set_rules('aPrice', 'Product Price', 'trim|number');
+		$this->fv->set_rules('aDescription', 'Product Description', 'trim|required');
+		$this->fv->set_rules('aCategory', 'Product Category', 'trim|required');
+		if($this->fv->run()==FALSE)
+			$response=['status'=>false, 'response'=>validation_errors()];
+		else
+		{
+			$data=null;
+			$data = [
+				'Name'=>$this->input->post('aName'),
+				'Price'=>$this->input->post('aPrice'),
+				'Description'=>$this->input->post('aDescription'),
+				'CategorySlug'=>$this->input->post('aCategory'),
+			];
+			$this->pm->add_product_d($data);
+			$response=['status'=>true, 'response'=>'Product Added successfully :)'];
+		}
+		echo json_encode($response);
 	}
 
 }
