3ca19aec17b25709666e596b18c8d2649d1616f3
HarshGundecha <harsh.gundecha@gmail.com>
2018-08-17 20:03:39 +0530

    added query for single and multiple product delete

--- application/models/Product_m.php
+++ application/models/Product_m.php
@@ -7,9 +7,10 @@ class Product_m extends MY_Model{
     if($where)
       $this->db->where($where);
     $data = $this->db
-      ->select('P.Name PName, P.Description, P.Price, C.Name CName')
+      ->select('P.Name PName, P.ProductSlug, P.Description, P.Price, C.Name CName')
       ->from('product P')
       ->join('category C', 'P.CategorySlug=C.CategorySlug')
+      ->where(['Status'=>1])
       ->get()
       ->result();
     return $data;
@@ -19,6 +20,22 @@ class Product_m extends MY_Model{
   {
     $this->db
       ->insert('product', $data);
+  }
+
+  public function delete_product_d($condition)
+  {
+    $this->db
+      ->set(['Status'=>0])
+      ->where($condition)
+      ->update('product');
+  }
+
+  public function delete_multiple_product_d($condition)
+  {
+    $this->db
+      ->set(['Status'=>0])
+      ->where_in('ProductSlug',$condition)
+      ->update('product');
   }
 
   public function get_category_d()
